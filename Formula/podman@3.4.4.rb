2d227416352b2d298f71a94bac0b6a40a30f832c
Anqi Pang <anqipans@amazon.com>
2022-04-07 12:52:56 -0700

    add podman 3.4.4

---
+++ Formula/podman@3.4.4.rb
@@ -0,0 +1,69 @@
+class PodmanAT344 < Formula
+  desc "Tool for managing OCI containers and pods"
+  homepage "https://podman.io/"
+  license "Apache-2.0"
+
+  stable do
+    url "https://github.com/containers/podman/archive/v3.4.4.tar.gz"
+    sha256 "718c9e1e734c2d374fcf3c59e4cc7c1755acb954fc7565093e1d636c04b72e3a"
+
+    resource "gvproxy" do
+      url "https://github.com/containers/gvisor-tap-vsock/archive/v0.3.0.tar.gz"
+      sha256 "6ca454ae73fce3574fa2b615e6c923ee526064d0dc2bcf8dab3cca57e9678035"
+    end
+  end
+
+  head do
+    url "https://github.com/containers/podman.git", branch: "main"
+
+    resource "gvproxy" do
+      url "https://github.com/containers/gvisor-tap-vsock.git", branch: "main"
+    end
+  end
+
+  depends_on "go" => :build
+  depends_on "go-md2man" => :build
+  depends_on "qemu"
+
+  def install
+    ENV["CGO_ENABLED"] = "1"
+    os = OS.kernel_name.downcase
+
+    inreplace "vendor/github.com/containers/common/pkg/config/config_#{os}.go",
+              "/usr/local/libexec/podman",
+              libexec
+
+    system "make", "podman-remote-#{os}"
+    if OS.mac?
+      bin.install "bin/#{os}/podman" => "podman-remote"
+      bin.install_symlink bin/"podman-remote" => "podman"
+    else
+      bin.install "bin/podman-remote"
+    end
+
+    resource("gvproxy").stage do
+      system "make", "gvproxy"
+      libexec.install "bin/gvproxy"
+    end
+
+    if build.head?
+      system "make", "podman-remote-#{os}-docs"
+    else
+      system "make", "install-podman-remote-#{os}-docs"
+    end
+
+    man1.install Dir["docs/build/remote/#{os}/*.1"]
+
+    bash_completion.install "completions/bash/podman"
+    zsh_completion.install "completions/zsh/_podman"
+    fish_completion.install "completions/fish/podman.fish"
+  end
+
+  test do
+    assert_match "podman-remote version #{version}", shell_output("#{bin}/podman-remote -v")
+    assert_match(/Cannot connect to Podman/i, shell_output("#{bin}/podman-remote info 2>&1", 125))
+
+    machineinit_output = shell_output("podman-remote machine init --image-path fake-testi123 fake-testvm 2>&1", 125)
+    assert_match "Error: open fake-testi123: no such file or directory", machineinit_output
+  end
+end
